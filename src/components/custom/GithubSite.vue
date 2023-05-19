d63c961aab118ccba1377dfad67b7ab730ed9444
jiucheng-kj <69421775+jiucheng-kj@users.noreply.github.com>
2023-05-19 15:04:14 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <a href="https://www.518tv.cn" target="_blank" class="text-blue-500">
+      久橙导航
     </a>
   </div>
 </template>
